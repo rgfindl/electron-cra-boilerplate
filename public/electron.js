ed0a8694a9b78ce0478f04224a04da4b8fb9414b
Yurii Timofeyev <georgius@ural.ru>
2019-04-08 22:59:21 +0500

    Dependency fixes and main process improvements

--- public/electron.js
+++ public/electron.js
@@ -1,33 +1,79 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const electron = require('electron')
+const { app, crashReporter, BrowserWindow, Menu } = electron
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+const path = require('path')
+const isDev = require('electron-is-dev')
 
-let mainWindow;
+let mainWindow
 
-function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  if (isDev) {
-    // Open the DevTools.
-    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    mainWindow.webContents.openDevTools();
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer')
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+  for (const name of extensions) {
+    try {
+      await installer.default(installer[name], forceDownload)
+    } catch (e) {
+      console.log(`Error installing ${name} extension: ${e.message}`)
+    }
   }
-  mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
+crashReporter.start({
+  productName: 'YourName',
+  companyName: 'YourCompany',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  uploadToServer: false
+})
+
+function createWindow() {
+  if (isDev) {
+    installExtensions()
+  }
+
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      backgroundThrottling: false
+    }
+  })
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+  mainWindow.webContents.once('did-finish-load', () => {
+    mainWindow.show()
+    if (isDev) {
+      // Open the DevTools.
+      // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+      mainWindow.webContents.openDevTools()
+      // add inspect element on right click menu
+      mainWindow.webContents.on('context-menu', (e, props) => {
+        Menu.buildFromTemplate([
+          {
+            label: 'Inspect element',
+            click() {
+              mainWindow.inspectElement(props.x, props.y)
+            }
+          }
+        ]).popup(mainWindow)
+      })
+    }
+  })
+
+  mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
